0ce7da446cdc5980160ee46e5a507651bf96ca8a
Siegescript_ <143763707+Siegescript@users.noreply.github.com>
2026-04-15 23:27:14 +0800

    feat: created divided differences function

--- div-diff.r
+++ div-diff.r
@@ -1,1 +1,13 @@
-print("Hello World!")
+# ====================== DIVIDED DIFFERENCES ALGORITHM ======================
+newton_divided_diff <- function(x, y) {
+  n <- length(x)
+  dd <- matrix(0, nrow = n, ncol = n)
+  dd[, 1] <- y                    # zeroth-order differences
+  
+  for (j in 2:n) {                # j = order + 1
+    for (i in 1:(n - j + 1)) {
+      dd[i, j] <- (dd[i + 1, j - 1] - dd[i, j - 1]) / (x[i + j - 1] - x[i])
+    }
+  }
+  return(dd)
+}
